81c9ed24479f6edb7a9a299a5dabc3317de89426
macintosh <ameed.ansari@creatrixe.com>
2020-06-28 19:49:59 -0700

    initial commit

---
+++ transformers/ServerManager.swift
@@ -0,0 +1,9 @@
+//
+//  ServerManager.swift
+//  transformers
+//
+//  Created by macintosh on 2020-06-28.
+//  Copyright © 2020 aequilibrium. All rights reserved.
+//
+
+import Foundation
---
+++ transformers/UtilManager.swift
@@ -0,0 +1,9 @@
+//
+//  UtilManager.swift
+//  transformers
+//
+//  Created by macintosh on 2020-06-28.
+//  Copyright © 2020 aequilibrium. All rights reserved.
+//
+
+import Foundation
